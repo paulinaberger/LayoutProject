f999b8d16c6d86239543595c2ebb4f92a2c47806
Paulina Berger <paulina.berger@gmail.com>
2017-02-19 20:44:30 +0100

    adding so many changes

--- app/src/main/java/org/mdsd2016/android/layoutproject/MainActivity.java
+++ app/src/main/java/org/mdsd2016/android/layoutproject/MainActivity.java
@@ -1,7 +1,9 @@
 package org.mdsd2016.android.layoutproject;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.Passport);
+        textView.setPaintFlags(textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
